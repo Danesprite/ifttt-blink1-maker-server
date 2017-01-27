5839cb4dbc1bb0bc09255ccc678695050797603b
Dane Finlay <Danesprite@gmail.com>
2017-01-27 17:35:42 +1100

    Included specifics for blink1

--- simplerequestserver.js
+++ simplerequestserver.js
@@ -4,11 +4,15 @@ var app = express();
 const hostname = '127.0.0.1';
 const port = 8080;
 
-app.post('/dosomething', function (req, res) {
-  // Do something
+app.post('/blink', function (req, res) {
+  // TODO might want to actually look at the request body for the colour to use
+  
+
+  // make the blink1 do something
+  exec('blink1-tool --blink 1');
 
   // Send a response back stating that the request was successful
-  res.send('Request successful!');
+  res.send('blink1 request successful!');
 });
 
 
