117e3e622fca94085c2045e72ca36fbc7bfcc914
Danesprite <Danesprite@gmail.com>
2017-01-27 17:25:19 +1100

    Update simplerequestserver.js
    removed blink(1) specific code

--- simplerequestserver.js
+++ simplerequestserver.js
@@ -4,15 +4,11 @@ var app = express();
 const hostname = '127.0.0.1';
 const port = 8080;
 
-app.post('/blink', function (req, res) {
-  // TODO might want to actually look at the request body for the colour to use
-  
-
-  // make the blink1 do something
-  exec('blink1-tool --blink 1');
+app.post('/dosomething', function (req, res) {
+  // Do something
 
   // Send a response back stating that the request was successful
-  res.send('blink1 request successful!');
+  res.send('Request successful!');
 });
 
 
